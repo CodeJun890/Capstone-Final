89f3a5d63312072361dc041219908d88e0fd015b
Arnulfo Villareal Jr <arnulfov8900@gmail.com>
2024-01-03 18:09:25 +0800

    asd

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { Loading } from "./Components/Homepage/Loading.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// const App = React.lazy(() => import("./App.jsx"));
-import App from "./App.jsx";
+const App = React.lazy(() => import("./App.jsx"));
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "aos/dist/aos.css";
@@ -13,7 +12,14 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<App />} />
+        <Route
+          path="*"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <App />
+            </React.Suspense>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
