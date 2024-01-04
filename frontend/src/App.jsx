96d120df9559864121101b5175e337d95f865343
Arnulfo Villareal Jr <arnulfov8900@gmail.com>
2024-01-04 11:14:48 +0800

    asd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -129,7 +129,7 @@ export default function App() {
         <Routes>
           {/* Homepage Routes */}
           <Route
-            path="/*"
+            path="*"
             element={
               isAdminLoggedIn == "true" ? (
                 <Admin toggleAdminIsLoggedOut={toggleAdminIsLoggedOut} />
