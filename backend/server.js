3cbef6a88d62b3218ec9d3c44ac093e950d12768
Arnulfo Villareal Jr <arnulfov8900@gmail.com>
2024-01-04 13:05:51 +0800

    asd

--- backend/server.js
+++ backend/server.js
@@ -170,10 +170,6 @@ const verifyUserStudent = (req, res, next) => {
   }
 };
 
-app.get("*", (req, res) => {
-  res.redirect(path.join(__dirname, "..", "frontend/dist", "index.html"));
-});
-
 app.get("/admin", verifyUserAdmin, (req, res) => {
   const admin = req.adminUser;
   res.status(200).json({ adminUser: admin });
