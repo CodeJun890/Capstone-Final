ac2dd4a4d983b3a9747c2b9ccb871fc2b37b9fee
Arnulfo Villareal Jr <arnulfov8900@gmail.com>
2024-01-04 20:53:05 +0800

    asd

--- frontend/src/Components/Homepage/ForgotPass.jsx
+++ frontend/src/Components/Homepage/ForgotPass.jsx
@@ -24,7 +24,6 @@ export default function ForgotPass() {
         })
         .catch((err) => {
           setMessage(err);
-          console.error(err);
         });
     } else {
       formRef.current.reportValidity();
