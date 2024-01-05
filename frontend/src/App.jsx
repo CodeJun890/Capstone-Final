323b5aba517d1cd3be22feac2770b63cab1951e2
Arnulfo Villareal Jr <arnulfov8900@gmail.com>
2024-01-05 19:35:01 +0800

    asd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,6 +62,7 @@ export default function App() {
     if (!isRecognizedRoute) {
       handleUnrecognizedRoute();
     }
+    console.log("hehe");
   }, [isRecognizedRoute]);
 
   const isRecognizedRoute =
