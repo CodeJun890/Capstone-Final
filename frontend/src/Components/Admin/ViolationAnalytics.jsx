efd8db64d1767b921c7a9405e6e697524bb53eac
Arnulfo Villareal Jr <arnulfov8900@gmail.com>
2024-01-25 02:40:16 +0800

    sad

--- frontend/src/Components/Admin/ViolationAnalytics.jsx
+++ frontend/src/Components/Admin/ViolationAnalytics.jsx
@@ -33,7 +33,6 @@ export default function ViolationAnalytics() {
 
         if (response.status === 200) {
           setCourses(response.data.courses);
-          console.log(response.data);
         }
       } catch (error) {
         console.log(error);
